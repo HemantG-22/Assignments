bad425630dc84c141bf061707946e176f38777ea
Hemant Gandhre <hmntgandhre@gmail.com>
2026-05-27 08:49:54 +0530

    Assignment 1

---
+++ javascript-training/language-basics/first.-js-program.js
@@ -0,0 +1,7 @@
+console.log("Hello, World..!");
+
+//different types of print statements
+console.info("This is info message");
+console.warn("This is warning message");
+console.error("This is an error");
+
---
+++ typescript-training/Assignments/assignment1.ts
@@ -0,0 +1,4 @@
+//Temprature of city in degree. 
+let temprature : number = 35.5;
+console.log(`Temprature of the city is ${temprature}`);
+
