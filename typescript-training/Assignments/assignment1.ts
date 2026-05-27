bbe592c101464b23eeda013ca8f882bc951bf81f
Hemant Gandhre <hmntgandhre@gmail.com>
2026-05-28 00:25:08 +0530

    Assignmenet1&2

--- typescript-training/Assignments/assignment1.ts
+++ typescript-training/Assignments/assignment1.ts
@@ -2,3 +2,78 @@
 let temprature : number = 35.5;
 console.log(`Temprature of the city is ${temprature}`);
 
+//Whether a customer has placed an order: true or false
+let orderplaced : boolean = true;
+console.log(`Is order Place ${orderplaced}`);
+
+//Person's phone number: "123-456-7890"
+let phonenumber : number = 9766688031;
+console.log(`Phone number is ${phonenumber}`);
+
+//Amount of money in a customer's bank account: 1000.50
+let balance : number = 10000.50;
+console.log(`Account balance is ${balance}`);
+
+//Person's email address: "john.doe@example.com"
+let personemail : string = "hmntgandhre@gmai.com";
+console.log(`User email is ${personemail}`);
+
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates : number [] = [37.7749, -122.4194];
+console.log("Coordinates are"+" "+coordinates[0]+ " "+coordinates[1]);
+
+//Person's marital status: true or false
+let maritalstatus : boolean = true;
+console.log(`Marital statu of person is ${maritalstatus}`);
+
+//Person's occupation: "Software Engineer"
+let occupation : string = "Software Engineer";
+console.log(`User occupation is ${occupation}`);
+
+//Person's favourite colour: "Blue"
+let favoritecolor : string = "Blue";
+console.log("User favorite color is"+ " "+favoritecolor);
+
+//Current year: 2027
+let currentyear : number = 2027;
+console.log(`Current year is ${currentyear}`);
+
+//Number of followers on a social media platform: 1,000,000
+let followers : number = 1000000;
+console.log(`Social Media Followers count is ${followers}`);
+
+//Rating of a movie: 7.5
+let movierating : number = 7.5;
+console.log("Mobie Ratings are"+" "+movierating);
+
+//Person's blood type: 'A'
+let bloodgroup : string = 'A';
+console.log("User blood group is"+" "+bloodgroup);
+
+//Title of a book: "To Kill a Mockingbird"
+let booktitle : string = "To Kill a Mockingbird";
+console.log(`User Favorite Book Title is ${booktitle}`);
+
+//Number of employees in a company: 500
+let employeecount : number = 500;
+console.log(`Total Employee Count in Company is ${employeecount}`);
+
+//Time of an event: 2:30 PM
+let eventtime : string = "2:30 PM";
+console.log("Event Timeing is"+ " "+eventtime);
+
+//Name of a country: "United States"
+let country : string = "United States";
+console.log(`Country is ${country}`);
+
+//Person's eye color: "Brown"
+let eyecolor : string = "Brown";
+console.log(`User Eye Color is ${eyecolor}`);
+
+//Person's birthplace: "New York City"
+let birthplace : string = "New York City";
+console.log(`User Birth Place is ${birthplace}`);
+
+//Distance between two cities: 200.5
+let citydistance : number = 200.5;
+console.log(`Distance between two cities is ${citydistance}`);
---
+++ typescript-training/Assignments/assignment2.ts
@@ -0,0 +1,64 @@
+/* Decleared one function with return type as (string) to check the loan eligibility, Inside the function, Checked user eligibility with nested ifelse conditional statments And then pass the input values which are decleared in function,
+Stored function return values in result veriable by calling the function
+And Print the result veriable. */
+
+function checkLoanEligibility (
+  customerName : string,
+  creditScore : number,
+  income : number,
+  isEmployed : boolean,
+  debtToIncomeRatio : number
+)
+: string {
+  
+  // Rule 1: Credit Score Check
+  if (creditScore > 750) {
+    return `${customerName} is eligible for the loan (Auto Approved).`;
+  } 
+  else if (creditScore >= 650 && creditScore <= 750) {
+
+    // Rule 2: Income Check
+    if (income >= 50000) {
+
+      // Rule 3: Employment Check
+      if (isEmployed) {
+
+        // Rule 4: Debt-to-Income Ratio Check
+        if (debtToIncomeRatio < 40) {
+          return `${customerName} is eligible for the loan.`;
+        } else {
+          return `${customerName} is not eligible for the loan due to high DTI ratio.`;
+        }
+
+      } else {
+        return `${customerName} is not eligible for the loan because the customer is unemployed.`;
+      }
+
+    } else {
+      return `${customerName} is not eligible for the loan due to insufficient income.`;
+    }
+
+  } 
+  else {
+    return `${customerName} is not eligible for the loan due to low credit score.`;
+  }
+}
+
+// Customer Details
+let customerName = "John Doe";
+let creditScore = 720;
+let income = 55000.0;
+let isEmployed = true;
+let debtToIncomeRatio = 35.0;
+
+// Function Call
+const result = checkLoanEligibility(
+  customerName,
+  creditScore,
+  income,
+  isEmployed,
+  debtToIncomeRatio
+);
+
+// Print Result
+console.log(result);
